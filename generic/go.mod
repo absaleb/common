fac578ce6ac2c3d3f13748c551742736ffa7b2d4
abs <abs@okta-solutions.com>
2021-04-06 00:44:43 +0300

    added generic folder

---
+++ generic/go.mod
@@ -0,0 +1,3 @@
+module github.com/generic
+
+go 1.16
---
+++ generic/util.go
@@ -0,0 +1,15 @@
+package common
+
+import "os"
+
+func GetEnv(key, defaultVal string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	} else {
+		return defaultVal
+	}
+}
+
+func GetVersion() string {
+	return "v1.0.10"
+}
